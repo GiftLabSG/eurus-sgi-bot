3988d2081753fe80d863a4bd64dbdfbd5f17f265
GiftLabSG <giftlabsg@gmail.com>
2025-08-06 15:07:28 +0000

    Edited main.py format fallback message

--- main.py
+++ main.py
@@ -53,10 +53,8 @@ if query:
             st.exception(e)
             st.info(
 """If you're looking for training or workforce upgrading support, you might consider grants such as:
-
 - **Career Conversion Programme (CCP) for Security Officers**
 - **Company Training Committee Grant (CTC)**
 - **Productivity Solutions Grant (PSG)**
-
 📬 For more help, you can contact **WSG_Biz_Services@wsg.gov.sg** or [fill out this form](https://go.gov.sg/contact-form)."""
             )
